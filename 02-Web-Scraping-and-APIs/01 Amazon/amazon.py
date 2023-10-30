5b30a6c05df2e16399afd0e2031e04c256132ed2
Florian Kellner <37459269+kef59000@users.noreply.github.com>
2023-10-30 22:18:59 +0100

    user agent

--- 02-Web-Scraping-and-APIs/01 Amazon/amazon.py
+++ 02-Web-Scraping-and-APIs/01 Amazon/amazon.py
@@ -12,6 +12,8 @@ import smtplib
 # Connect to Website and pull in data
 URL = 'https://www.amazon.com/Funny-Data-Systems-Business-Analyst/dp/B07FNW9FGJ/ref=sr_1_3?dchild=1&keywords=data%2Banalyst%2Btshirt&qid=1626655184&sr=8-3&customId=B0752XJYNL&th=1'
 
+# Find Your User-Agent: https://httpbin.org/get
+
 headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
 
 page = requests.get(URL, headers=headers)
